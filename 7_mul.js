7c2009ddf580e7733b9cbb61f5b85998c1b97f68
Cristioro <69018012+Cristioro@users.noreply.github.com>
2024-06-21 13:42:17 -0500

    Add files via upload

---
+++ 7_mul.js
@@ -0,0 +1,25 @@
+const mul = +document.getElementById("inputMul")
+const btnMul = document.getElementById("btnMul")
+const ResultTheShow = document.querySelectorAll("#show")
+
+const multiplication = function() {
+
+  for (let i = 1; i < 11; i++) {
+      let result = mul * i;
+      let info = [mul, i, result];
+    
+      for(let r = 0; r < 30; r++) {
+      
+      for (let v = 0; v < 2; v++) {
+        const elementShow = ResultTheShow[r];
+        const elementInfo = info[v]
+        console.log(elementShow)
+        
+        elementShow.innerHTML = elementInfo
+     }
+    }
+    
+  }
+}
+
+btnMul.addEventListener("click", multiplication)
